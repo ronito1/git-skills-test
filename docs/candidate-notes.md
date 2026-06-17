09983e463e3cbde6c7ab45b32f0719f1a7840172
ronito1 <ronitpathak12345@gmail.com>
2026-06-17 10:37:09 +0530

    Revert "Merge pull request #2 from ronito1/feature/add-priority"
    This reverts commit 8918dbe1879f8324009512b9818258bd9dc44616, reversing
    changes made to 926ed56accd759367e4c4d7992ed8e8b67ffdf69.

--- docs/candidate-notes.md
+++ docs/candidate-notes.md
@@ -2,6 +2,6 @@
 
 Please replace this text with:
 
-- Full name: Vasu Chourasia
-- Branch name: `feature/add-priority
-- Change summary: added priority field in services.json file and made changes in the print-services script file
+- Full name:
+- Branch name:
+- Change summary:
--- scripts/print-services.js
+++ scripts/print-services.js
@@ -5,6 +5,6 @@ const filePath = path.join(__dirname, "..", "data", "services.json");
 const services = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
 for (const service of services) {
-  const {priority} = service;
-  console.log(`${service.name} | owner=${service.owner} |  priority=${priority}`);
+  const priority = service.priority || "missing";
+  console.log(`${service.name} | owner=${service.owner} | priority=${priority}`);
 }
